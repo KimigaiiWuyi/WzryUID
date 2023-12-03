4c878f6090d567d3c1db1871ba574d1445e75e18
Kloping <3474006766@qq.com>
2023-12-03 19:23:48 +0800

    kloping: 新增查荣耀过滤2

--- WzryUID/wzryuid_battle_history/__init__.py
+++ WzryUID/wzryuid_battle_history/__init__.py
@@ -8,17 +8,7 @@ from ..utils.database.models import WzryBind
 from ..utils.error_reply import get_error
 
 sv_wzry_history = SV('查询王者荣耀战绩')
-sv_wzry_history_filter = SV('查询王者荣耀战绩')
-
-
-@sv_wzry_history.on_command(('查荣耀'))
-async def send_wzry_history(bot: Bot, ev: Event):
-    uid = await get_uid(bot, ev, WzryBind)
-    if uid is None:
-        return await bot.send(get_error(-41))
-    logger.info(f'[查荣耀] uid:{uid}')
-    im = await draw_history_img(ev.user_id, uid)
-    await bot.send(im)
+sv_wzry_history_filter = SV('查询王者荣耀战绩', priority=10)
 
 
 @sv_wzry_history_filter.on_prefix(('查荣耀'))
@@ -36,4 +26,14 @@ async def send_wzry_history(bot: Bot, ev: Event):
         option = 5
     logger.info(f'[查荣耀] uid:{uid}')
     im = await draw_history_img(ev.user_id, uid, option)
+    await bot.send(im)
+
+
+@sv_wzry_history.on_command(('查荣耀'))
+async def send_wzry_history(bot: Bot, ev: Event):
+    uid = await get_uid(bot, ev, WzryBind)
+    if uid is None:
+        return await bot.send(get_error(-41))
+    logger.info(f'[查荣耀] uid:{uid}')
+    im = await draw_history_img(ev.user_id, uid)
     await bot.send(im)
