199ce41f6c0c2efc491892f92d57ed8b7b7a0f3e
Wuyi无疑 <444835641@qq.com>
2023-12-03 20:00:45 +0800

    🔥 移除多余触发器

--- WzryUID/wzryuid_battle_history/__init__.py
+++ WzryUID/wzryuid_battle_history/__init__.py
@@ -8,11 +8,9 @@ from ..utils.error_reply import get_error
 from ..utils.database.models import WzryBind
 from .draw_battle_history import draw_history_img
 
-sv_wzry_history_filter = SV('查询王者荣耀战绩', priority=10)
 sv_wzry_history = SV('查询王者荣耀战绩')
 
-
-@sv_wzry_history_filter.on_prefix(('查荣耀'))
+@sv_wzry_history.on_command(('查荣耀'))
 async def send_wzry_history(bot: Bot, ev: Event):
     uid = await get_uid(bot, ev, WzryBind)
     if uid is None:
@@ -27,14 +25,4 @@ async def send_wzry_history(bot: Bot, ev: Event):
         option = 4
     logger.info(f'[查荣耀] uid:{uid}')
     im = await draw_history_img(ev.user_id, uid, option)
-    await bot.send(im)
-
-
-@sv_wzry_history.on_command(('查荣耀'))
-async def send_wzry_history(bot: Bot, ev: Event):
-    uid = await get_uid(bot, ev, WzryBind)
-    if uid is None:
-        return await bot.send(get_error(-41))
-    logger.info(f'[查荣耀] uid:{uid}')
-    im = await draw_history_img(ev.user_id, uid)
     await bot.send(im)
