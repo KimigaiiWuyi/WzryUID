6bfaa1c2bd00d9e3b280b9b6612c18ae9b79b1ea
KimigaiiWuyi <444835641@qq.com>
2025-08-13 05:02:17 +0800

    🐛 修复一些BUG (#25)

--- WzryUID/utils/error_reply.py
+++ WzryUID/utils/error_reply.py
@@ -1,4 +1,20 @@
+error_code = {
+    -41: '你还没有绑定过uid哦!\n请使用[王者绑定UID]命令绑定!',
+    -51: '你还没有绑定过CK哦!\n请使用[王者添加CK]命令绑定!',
+    -61: '数据库不存在可用CK\n请使用[王者添加CK]命令绑定第一个CK!',
+    -30032: '用户不存在, 请检查是否绑定正确的UID(营地ID)\n可以使用[王者删除UID]命令删除错误的UID!',
+    -30003: 'CK已失效, 请重新绑定。',
+    -20011: '参数异常，请检查源代码...',
+    -30087: '该用户暂无角色...请检查UID是否正确。',
+    -47004: '该召唤师隐藏了皮肤列表, 无法查看。',
+}
+
+
+
 def get_error(retcode: int) -> str:
+    if retcode in error_code:
+        return error_code[retcode]
+
     if retcode == -41:
         return '你还没有绑定过uid哦!\n请使用[王者绑定UID]命令绑定!'
     elif retcode == -51:
